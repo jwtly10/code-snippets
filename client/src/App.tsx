3bf474d98a62f6cd940dfc946888e680e558c964
Josh Watley <watleyjosh@gmail.com>
2023-08-28 18:47:24 +0100

    Adding sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Button, Container } from 'react-bootstrap'
 import { confirmAlert } from 'react-confirm-alert'
 import AddEditSnippet from './components/AddEditSnippet'
+import { Sidebar, SubMenu, Menu, MenuItem } from 'react-pro-sidebar'
 
 function App() {
     function handleNewSnippet() {
@@ -18,11 +19,38 @@ function App() {
     }
     return (
         <>
-            <Container>
-                <h1>code-snippets</h1>
-                <Button onClick={handleNewSnippet}>Add Snippet</Button>
-                <ListSnippets />
-            </Container>
+            <div style={{ display: 'flex', height: '100%' }}>
+                <Sidebar backgroundColor="#1F1F21">
+                    <Menu
+                        menuItemStyles={{
+                            button: {
+                                backgroundColor: '#1F1F21',
+                                '&:hover': {
+                                    backgroundColor: '#3c3c3f',
+                                },
+                            },
+                        }}
+                    >
+                        <div className="d-flex flex-column justify-content-center p-3">
+                            <h2 className="mb-3">code-snippets</h2>
+                            <Button
+                                className="btn btn-primary w-100"
+                                onClick={handleNewSnippet}
+                            >
+                                New Snippet
+                            </Button>
+                        </div>
+
+                        <MenuItem>All Snippets</MenuItem>
+                        <SubMenu label="Folders">
+                            <MenuItem>jfdskjfsldkj</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </Sidebar>
+                <Container>
+                    <ListSnippets />
+                </Container>
+            </div>
         </>
     )
 }
